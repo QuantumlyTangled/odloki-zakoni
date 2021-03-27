c54b72a92f70c28e4d3175439f0e06c1064c6036
QuantumlyTangled <ethoarx@gmail.com>
2021-03-27 22:43:07 +0100

    chore: configs

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	purge: [],
+	darkMode: 'media', // or 'media' or 'class'
+	theme: {
+		container: {
+			center: true
+		},
+		colors: {
+			dark: '#303030',
+			'dark-segment': '#272727',
+			'dark-text': '#ccc'
+		}
+	},
+	variants: {
+		extend: {}
+	},
+	plugins: []
+};
