8bc20fe149071a16f1aa558b18d8b16f3dd6af4c
QuantumlyTangled <ethoarx@gmail.com>
2021-03-27 21:49:42 +0100

    feat: prikaz grba

--- frontend/src/components/Obcina.tsx
+++ frontend/src/components/Obcina.tsx
@@ -1,16 +1,25 @@
 import React from 'react';
+import Image from 'next/image';
 
 export interface ObcinaProps {
 	ime: string;
+	grb?: string;
 }
 
-const Obcina: React.FC<ObcinaProps> = ({ ime }) => {
+const Obcina: React.FC<ObcinaProps> = ({ ime, grb }) => {
 	return (
 		<>
 			<div className="p-6 max-w-sm mx-auto bg-white dark:bg-gray-700 rounded-xl shadow-md flex items-center space-x-4">
 				<div className="flex-shrink-0">
-					{/* TODO: Dinamična slika grba */}
-					<img className="h-12 w-12" src="/img/logo.svg" alt={`Grb ${ime}`} />
+					{grb ? (
+						<Image //
+							src={`/grbi/${grb}`}
+							alt={`Grb ${ime}`}
+							width={64}
+							height={64}
+							className="h-12 w-12"
+						/>
+					) : null}
 				</div>
 				<div>
 					<div className="text-xl font-medium text-black dark:text-white">{ime}</div>
