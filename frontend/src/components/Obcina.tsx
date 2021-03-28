363146134c57777f68b7969d2280d7288a134647
QuantumlyTangled <ethoarx@gmail.com>
2021-03-28 14:43:08 +0200

    refactor: naredi gump kot povezavo do obcine

--- frontend/src/components/Obcina.tsx
+++ frontend/src/components/Obcina.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { computeGrb } from 'utils/constants/Grbi';
-import type { Obcine } from 'utils/constants/Obcine';
+import { Obcine } from 'utils/constants/Obcine';
 
 export interface ObcinaProps {
 	obcina: Obcine;
@@ -12,23 +13,25 @@ const Obcina: React.FC<ObcinaProps> = ({ obcina }) => {
 
 	return (
 		<>
-			<div className="p-6 max-w-sm mx-auto bg-white dark:bg-dark-segment rounded-xl shadow-md flex items-center space-x-4 select-none cursor-pointer">
-				<div className="flex-shrink-0">
-					{grbLokacija ? (
-						<div className="h-12 w-12 relative">
-							<Image //
-								src={`/grbi/${grbLokacija}`}
-								alt={`Grb ${obcina}`}
-								layout="fill"
-								objectFit="contain"
-							/>
-						</div>
-					) : null}
+			<Link href={`/obcina/${Obcine[obcina]}`}>
+				<div className="p-6 max-w-sm mx-auto bg-white dark:bg-dark-segment rounded-xl shadow-md flex items-center space-x-4 select-none cursor-pointer">
+					<div className="flex-shrink-0">
+						{grbLokacija ? (
+							<div className="h-12 w-12 relative">
+								<Image //
+									src={`/grbi/${grbLokacija}`}
+									alt={`Grb ${obcina}`}
+									layout="fill"
+									objectFit="contain"
+								/>
+							</div>
+						) : null}
+					</div>
+					<div>
+						<div className="text-xl font-medium text-black dark:text-dark-text">{obcina}</div>
+					</div>
 				</div>
-				<div>
-					<div className="text-xl font-medium text-black dark:text-dark-text">{obcina}</div>
-				</div>
-			</div>
+			</Link>
 		</>
 	);
 };
