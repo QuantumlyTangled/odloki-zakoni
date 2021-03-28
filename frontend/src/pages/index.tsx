694dd8555019f62393ef173a99087886ed9f54ea
QuantumlyTangled <ethoarx@gmail.com>
2021-03-28 11:19:28 +0200

    refactor: spremeni seznam v komponento

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,5 @@
+import SeznamObcin from 'components/SeznamObcin';
 import React from 'react';
-import { Obcine } from 'utils/constants/Obcine';
-import Obcina from '../components/Obcina';
 
 export default function Home() {
 	return (
@@ -11,26 +10,7 @@ export default function Home() {
 					<br />
 					<h2>Medobčinska inšpekcija</h2>
 				</div>
-				<div className="grid grid-cols-3 gap-4">
-					<Obcina obcina={Obcine.Maribor} />
-					<Obcina obcina={Obcine.Benedikt} />
-					<Obcina obcina={Obcine.Cerkvenjak} />
-					<Obcina obcina={Obcine.Duplek} />
-					<Obcina obcina={Obcine.HoceSlivnica} />
-					<Obcina obcina={Obcine.Kungota} />
-					<Obcina obcina={Obcine.Lenart} />
-					<Obcina obcina={Obcine.LovrencNaPohorju} />
-					<Obcina obcina={Obcine.MiklavzNaDravskemPolju} />
-					<Obcina obcina={Obcine.Pesnica} />
-					<Obcina obcina={Obcine.RaceFram} />
-					<Obcina obcina={Obcine.Ruse} />
-					<Obcina obcina={Obcine.SelnicaObDravi} />
-					<Obcina obcina={Obcine.Starse} />
-					<Obcina obcina={Obcine.SvetaTrojica} />
-					<Obcina obcina={Obcine.SvetaAna} />
-					<Obcina obcina={Obcine.SvetiJurijVSlovenskihGoricah} />
-					<Obcina obcina={Obcine.Sentilj} />
-				</div>
+				<SeznamObcin />
 			</div>
 		</>
 	);
