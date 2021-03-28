ee1e3bf222fa7d46624d594d75133ecfac47f572
QuantumlyTangled <ethoarx@gmail.com>
2021-03-28 14:42:44 +0200

    feat: wildcard stran za vsako obcino

---
+++ frontend/src/pages/obcina/[id].tsx
@@ -0,0 +1,40 @@
+import type { GetStaticPropsContext, GetStaticPropsResult, NextPage } from 'next';
+import React from 'react';
+import { Obcine } from 'utils/constants/Obcine';
+
+export interface ObcinaPageProps {
+	obcina: string;
+	obcinaIme: Obcine;
+}
+
+const ObcinaPage: NextPage<ObcinaPageProps> = ({ obcinaIme }) => {
+	return <div>{obcinaIme}</div>;
+};
+
+export const getStaticProps = ({ params }: GetStaticPropsContext<{ id: string }>): GetStaticPropsResult<ObcinaPageProps> => {
+	return {
+		props: {
+			obcina: params!.id,
+			// @ts-expect-error Magic
+			obcinaIme: Obcine[params!.id]
+		}
+	};
+};
+
+export const computeSeznamObcin = () => {
+	return Object.keys(Obcine).map((obcina) => ({
+		params: {
+			id: obcina
+		}
+	}));
+};
+
+export const getStaticPaths = () => {
+	const paths = computeSeznamObcin();
+	return {
+		paths,
+		fallback: false
+	};
+};
+
+export default ObcinaPage;
