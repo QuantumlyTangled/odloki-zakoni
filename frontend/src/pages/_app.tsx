e16ae5e20dd12192d0fd4fae7c9eee838d94222c
QuantumlyTangled <ethoarx@gmail.com>
2021-04-11 16:30:12 +0200

    feat: SEO priprave

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,16 @@
 import Navbar from 'components/Navbar';
 import type { NextPage } from 'next';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import React from 'react';
 import '../styles/_App.css';
 
 const App: NextPage<AppProps> = ({ Component, pageProps }) => {
 	return (
 		<>
+			<Head>
+				<meta httpEquiv="Content-Type" content="text/html; charset=UTF-8" />
+			</Head>
 			<header>
 				<Navbar />
 			</header>
