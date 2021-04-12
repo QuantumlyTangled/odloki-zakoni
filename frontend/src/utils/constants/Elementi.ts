fedf11a1f4cb716d417fbce365cfa9ad5cd34cd1
QuantumlyTangled <ethoarx@gmail.com>
2021-04-12 17:30:43 +0200

    feat: se vec zakonov

--- frontend/src/utils/constants/Elementi.ts
+++ frontend/src/utils/constants/Elementi.ts
@@ -6,7 +6,13 @@ export const Elementi: Array<[Obcine, string[]]> = [
 		[
 			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520splo%25C5%25A1nem%2520upravnem%2520postopku%2520(ZUP)%252C.pdf',
 			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520prekr%25C5%25A1kih%2520(ZP-1)%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520in%25C5%25A1pekcijskem%2520nadzoru%2520(ZIN)%252C.pdf'
+			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520in%25C5%25A1pekcijskem%2520nadzoru%2520(ZIN)%252C.pdf',
+			// OKOLJE
+			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520varstvu%2520okolja%2520(ZVO-1)%252C.pdf',
+			// CESTE in PROMET
+			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520cestah%2520(ZCes-1)%252C.pdf',
+			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520prevozih%2520v%2520cestnem%2520prometu%2520(ZPCP-2)%252C.pdf',
+			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520rednem%2520vzdr%25C5%25BEevanju%2520javnih%2520cest%252C.pdf'
 		]
 	]
 ];
