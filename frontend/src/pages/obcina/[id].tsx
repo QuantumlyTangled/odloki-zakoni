5468e1e8c5e4279e36ae51025093585fd1825f11
QuantumlyTangled <ethoarx@gmail.com>
2021-04-12 19:40:49 +0200

    feat: prikaz kategorij

--- frontend/src/pages/obcina/[id].tsx
+++ frontend/src/pages/obcina/[id].tsx
@@ -1,7 +1,8 @@
+import Element from 'components/Element';
 import SeznamElementov from 'components/SeznamElementov';
 import type { GetStaticProps, NextPage } from 'next';
 import React from 'react';
-import { Elementi } from 'utils/constants/Elementi';
+import { Elementi, Kategorija } from 'utils/constants/Elementi';
 import { Obcine } from 'utils/constants/Obcine';
 
 export interface EleData {
@@ -13,7 +14,7 @@ export interface EleData {
 export interface ObcinaPageProps {
 	obcina: string;
 	obcinaIme: Obcine;
-	elementi: EleData[] | null;
+	elementi: (EleData | (Kategorija | URL[] | EleData[])[])[] | null;
 }
 
 const ObcinaPage: NextPage<ObcinaPageProps> = ({ obcinaIme, elementi }) => {
@@ -25,7 +26,27 @@ const ObcinaPage: NextPage<ObcinaPageProps> = ({ obcinaIme, elementi }) => {
 					<div className="grid">
 						<div className="flex-col flex-1 w-full p-5">
 							<div className="flex-col flex-1 mb-7">
-								<SeznamElementov naslov="test" elementi={elementi} />
+								<>
+									{elementi.map((ele) => {
+										if (Array.isArray(ele)) {
+											return (
+												<SeznamElementov //
+													key={`${ele[0]}`}
+													naslov={ele[0] as Kategorija}
+													elementi={ele[1] as EleData[]}
+												/>
+											);
+										}
+										return (
+											<Element //
+												key={`${ele.naslov}-${ele.st}`}
+												href={ele.href}
+												naslov={ele.naslov}
+												st={ele.st}
+											/>
+										);
+									})}
+								</>
 							</div>
 						</div>
 					</div>
@@ -37,21 +58,30 @@ const ObcinaPage: NextPage<ObcinaPageProps> = ({ obcinaIme, elementi }) => {
 
 // eslint-disable-next-line @typescript-eslint/require-await
 export const getStaticProps: GetStaticProps<ObcinaPageProps, { id: string }> = async ({ params }) => {
-	const elementi: EleData[] | null = Elementi.some((ele) => ele[0] === params!.id)
-		? Elementi.find((ele) => ele[0] === params!.id)![1]
-				.map((val) => new URL(val))
-				.map((val) => {
-					const partsUrl = val.pathname.split('/');
-					const partsEncoded = partsUrl[partsUrl.length - 1];
-					// Backblaze se enkrat enkodira url
-					const partsDecoded = decodeURIComponent(decodeURIComponent(partsEncoded));
-					const parts = partsDecoded.split(',');
+	const parseUrl = (url: URL): EleData => {
+		const partsUrl = url.pathname.split('/');
+		const partsEncoded = partsUrl[partsUrl.length - 1];
+		// Backblaze se enkrat enkodira url
+		const partsDecoded = decodeURIComponent(decodeURIComponent(partsEncoded));
+		const parts = partsDecoded.split(',');
 
-					return {
-						href: val.href,
-						naslov: parts[0],
-						st: parts[1].replace(/.[^.]+$/, '')
-					};
+		return {
+			href: url.href,
+			naslov: parts[0],
+			st: parts[1].replace(/.[^.]+$/, '')
+		};
+	};
+
+	const elementi = Elementi.has(params?.id as Obcine)
+		? Elementi.get(params!.id as Obcine)!
+				.map((val: [kategorija: Kategorija, elementi: string[]] | string) =>
+					Array.isArray(val) ? [val[0], val[1].map((dVal) => new URL(dVal))] : new URL(val)
+				)
+				.map((val) => {
+					if (Array.isArray(val)) {
+						return [val[0], (val[1] as URL[]).map((ele) => parseUrl(ele))];
+					}
+					return parseUrl(val);
 				})
 		: null;
 
--- frontend/src/utils/constants/Elementi.ts
+++ frontend/src/utils/constants/Elementi.ts
@@ -1,34 +1,60 @@
 import { Obcine } from './Obcine';
 
-export const Elementi: Array<[Obcine, string[]]> = [
+export enum Kategorija {
+	Okolje = 'Okolje',
+	CesteInPromet = 'Ceste in Promet',
+	SodneTakse = 'Sodne Takse',
+	PogrebnaDejavnost = 'Pogrebna Dejavnost',
+	PrebivaliscaInTurizen = 'Prebivališča in Turizem',
+	Gradnja = 'Gradnja'
+}
+
+export const Elementi = new Map<Obcine, Array<string | [Kategorija, string[]]>>([
 	[
 		Obcine.Zakonodaja,
 		[
 			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520splo%25C5%25A1nem%2520upravnem%2520postopku%2520(ZUP)%252C.pdf',
 			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520prekr%25C5%25A1kih%2520(ZP-1)%252C.pdf',
 			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520in%25C5%25A1pekcijskem%2520nadzoru%2520(ZIN)%252C.pdf',
-			// OKOLJE
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520varstvu%2520okolja%2520(ZVO-1)%252C.pdf',
-			// CESTE in PROMET
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520cestah%2520(ZCes-1)%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520prevozih%2520v%2520cestnem%2520prometu%2520(ZPCP-2)%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520rednem%2520vzdr%25C5%25BEevanju%2520javnih%2520cest%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520zaporah%2520na%2520cestah%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520gozdnih%2520prometnicah%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520prometni%2520signalizaciji%2520in%2520prometni%2520opremi%2520na%2520cestah%252C.pdf',
-			// SODNE TAKSE
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520upravnih%2520taksah%2520(ZUT)%252C.pdf',
-			// POGREBNA DEJAVNOST
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520pogrebni%2520in%2520pokopali%25C5%25A1ki%2520dejavnosti%2520(ZPPDej)%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520minimalnih%2520standardih%2520in%2520normativih%2520za%2520izvajanje%2520pogrebne%2520dejavnosti%252C.pdf',
-			// PREBIVALIŠČE IN TURIZEM
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520prijavi%2520prebivali%25C5%25A1%25C4%258Da%2520(ZPPreb-1)%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520spodbujanju%2520razvoja%2520turizma%2520(ZSRT-1)%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520prijavi%2520in%2520odjavi%2520gostov%252C.pdf',
-			// GRADNJA
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Gradbeni%2520zakon%2520(GZ)%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520univerzalni%2520graditvi%2520in%2520uporabi%2520objektov%252C.pdf',
-			'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Uredba%2520o%2520odvajanju%2520in%2520%25C4%258Di%25C5%25A1%25C4%258Denju%2520komunalne%2520odpadne%2520vode%252C.pdf'
+			[Kategorija.Okolje, ['https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520varstvu%2520okolja%2520(ZVO-1)%252C.pdf']],
+			[
+				Kategorija.CesteInPromet,
+				[
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520cestah%2520(ZCes-1)%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520prevozih%2520v%2520cestnem%2520prometu%2520(ZPCP-2)%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520rednem%2520vzdr%25C5%25BEevanju%2520javnih%2520cest%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520zaporah%2520na%2520cestah%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520gozdnih%2520prometnicah%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520prometni%2520signalizaciji%2520in%2520prometni%2520opremi%2520na%2520cestah%252C.pdf'
+				]
+			],
+			[
+				Kategorija.SodneTakse,
+				['https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520upravnih%2520taksah%2520(ZUT)%252C.pdf']
+			],
+			[
+				Kategorija.PogrebnaDejavnost,
+				[
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520pogrebni%2520in%2520pokopali%25C5%25A1ki%2520dejavnosti%2520(ZPPDej)%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520minimalnih%2520standardih%2520in%2520normativih%2520za%2520izvajanje%2520pogrebne%2520dejavnosti%252C.pdf'
+				]
+			],
+			[
+				Kategorija.PrebivaliscaInTurizen,
+				[
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520prijavi%2520prebivali%25C5%25A1%25C4%258Da%2520(ZPPreb-1)%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Zakon%2520o%2520spodbujanju%2520razvoja%2520turizma%2520(ZSRT-1)%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520prijavi%2520in%2520odjavi%2520gostov%252C.pdf'
+				]
+			],
+			[
+				Kategorija.Gradnja,
+				[
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Gradbeni%2520zakon%2520(GZ)%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Pravilnik%2520o%2520univerzalni%2520graditvi%2520in%2520uporabi%2520objektov%252C.pdf',
+					'https://f000.backblazeb2.com/file/odloki-zakoni/skupno/Uredba%2520o%2520odvajanju%2520in%2520%25C4%258Di%25C5%25A1%25C4%258Denju%2520komunalne%2520odpadne%2520vode%252C.pdf'
+				]
+			]
 		]
 	]
-];
+]);
